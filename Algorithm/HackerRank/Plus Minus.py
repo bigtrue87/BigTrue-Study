4ae2aabdab414186ed5f304025180fe7f108eeac
bigtrue87 <68408095+bigtrue87@users.noreply.github.com>
2020-07-25 22:31:15 +0900

    change

--- Algorithm/HackerRank/Plus Minus.py
+++ Algorithm/HackerRank/Plus Minus.py
@@ -46,7 +46,23 @@ import sys
 
 # Complete the plusMinus function below.
 def plusMinus(arr):
-    pass
+    plus = 0 
+    minus = 0 
+    zero = 0 
+    for i in range(len(arr)): 
+        if arr[i] == 0: 
+            zero = zero + 1 
+        elif arr[i] < 0: 
+            minus = minus + 1 
+        else: 
+            plus = plus + 1
+    len_arr = len(arr) 
+    minus_fraction = float(minus)/float(len_arr)
+    plus_fraction = float(plus) / float(len_arr)
+    zero_fraction = float(zero) / float(len_arr) 
+    print(plus_fraction) 
+    print(minus_fraction) 
+    print(zero_fraction)
 
 if __name__ == '__main__':
     n = int(input())
