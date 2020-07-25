c67f5d2a69495e0b2853b4a065d5bae615e98ff9
bigtrue87 <68408095+bigtrue87@users.noreply.github.com>
2020-07-25 23:35:53 +0900

    change

---
+++ Algorithm/HackerRank/Ice Cream Parlor.py
@@ -0,0 +1,15 @@
+t = int(input().strip())
+for a0 in range(t):
+    m = int(input().strip())
+    n = int(input().strip())
+    a = list(map(int, input().strip().split(' ')))
+ 
+    # effective cost
+    costList = {}
+    for i, cost in enumerate(a):
+        firUser = cost
+        secUser = m - cost
+        if secUser in costList.keys():
+            print(costList[secUser]+1,i+1)
+        else:
+            costList[cost] = i
