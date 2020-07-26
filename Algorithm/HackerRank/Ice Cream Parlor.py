2695efe911b348bbd04d645b64c2cd34c94f111a
bigtrue87 <68408095+bigtrue87@users.noreply.github.com>
2020-07-26 16:43:28 +0900

    change

--- Algorithm/HackerRank/Ice Cream Parlor.py
+++ Algorithm/HackerRank/Ice Cream Parlor.py
@@ -8,7 +8,7 @@ for a0 in range(t):
     costList = {}
     for i, cost in enumerate(a):
         firUser = cost
-        secUser = m - cost
+        secUser = m - cost 
         if secUser in costList.keys():
             print(costList[secUser]+1,i+1)
         else:
